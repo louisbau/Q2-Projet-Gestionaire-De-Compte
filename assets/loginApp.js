6f2beb4574abe46188e8d25e5f60b02103c8744a
bauchau <HE201881@students.ephec.be>
2021-03-14 18:17:15 +0100

    routing

---
+++ assets/loginApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import SignInSide from "./loginCode.js";
+ReactDOM.render(
+    <div>
+        <SignInSide />
+    </div>,
+    document.getElementById('teste')
+);
