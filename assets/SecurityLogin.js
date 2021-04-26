869301185192bdfd254b2d1a2a445ad489095277
bauchau <HE201881@students.ephec.be>
2021-04-26 14:48:48 +0200

    sign in

--- assets/SecurityLogin.js
+++ assets/SecurityLogin.js
@@ -6,6 +6,7 @@ import LoginSecurity from "./js/SecurityLoginCode";
 
 
 
+
 ReactDOM.render(
     <div>
         <LoginSecurity />
--- assets/js/SecurityLoginCode.js
+++ assets/js/SecurityLoginCode.js
@@ -9,7 +9,10 @@ import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import {Paper} from "@material-ui/core";
+import {Paper, useMediaQuery} from "@material-ui/core";
+import {palette} from "@material-ui/system";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -43,49 +46,66 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function LoginSecurity() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
     const classes = useStyles();
     return (
-        <Grid container component="main" className={classes.root}>
+        <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <Grid item xs={false} sm={4} md={7} className={classes.image} />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <form method="post" className={classes.paper}>
-                        <TextField variant="outlined"
-                                   margin="normal" fullWidth type="email" name="email" label="Email Address" id="inputEmail"
-                                   autoComplete={document.getElementById('last_username').value} placeholder="Email address"
-                                   required autoFocus/>
-                        <TextField
-                            variant="outlined"
-                            margin="normal" type="password" fullWidth label="Password" name="password" id="inputPassword" placeholder="Password" required/>
-                        <input type="hidden" id='token' name="_csrf_token"
-                               value={document.getElementById('token').value}
-                        />
-                        <div>
-                            <label>
+            <Grid container component="main" className={classes.root}>
 
-                                <input type="checkbox" name="_remember_me"/> Remember me
-                            </label>
-                        </div>
-                        <Button type="submit" fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}>
+                <Grid item xs={false} sm={4} md={7} className={classes.image} />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
                             Sign in
-                        </Button>
-                        <Link to='/signUp' href={"signUp"} variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
-                    </form>
-                </div>
+                        </Typography>
+                        <form method="post" className={classes.form} noValidate>
+                            <TextField variant="outlined"
+                                       margin="normal" fullWidth type="email" name="email" label="Email Address" id="inputEmail"
+                                       autoComplete={document.getElementById('last_username').value} placeholder="Email address"
+                                       required autoFocus/>
+                            <TextField
+                                variant="outlined"
+                                margin="normal" type="password" fullWidth label="Password" name="password" id="inputPassword" placeholder="Password" required/>
+                            <input type="hidden" id='token' name="_csrf_token"
+                                   value={document.getElementById('token').value}
+                            />
+                            <div>
+                                <label>
+
+                                    <input type="checkbox" name="_remember_me"/> Remember me
+                                </label>
+                            </div>
+                            <Button type="submit" fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}>
+                                Sign in
+                            </Button>
+                            <Link to='/signUp' href={"signUp"} variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </form>
+                    </div>
+                </Grid>
             </Grid>
-        </Grid>
+        </ThemeProvider>
+
+
 
     )
 }
